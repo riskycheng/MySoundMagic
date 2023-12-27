61ef86b08985c09fa82dd8d0ac3279b970622864
riskycheng <riskycheng@gmail.com>
2023-12-27 12:18:59 +0800

    add tabs for TTS and VC

--- MainGradio.py
+++ MainGradio.py
@@ -1,24 +1,35 @@
 import gradio as gr
 from gtts import gTTS
-import IPython.display as ipd
 import os
+import numpy as np
 
 
-# Define the function to generate speech
-def generate_speech(text):
-    tts = gTTS(text)
-    tts.save("output.mp3")
-    return "output.mp3"
+def flip_text(input_text):
+    return 'flip_text --> input_text'
 
 
-# Define the Gradio interface
+def flip_image(image):
+    return np.fliplr(image)
+
+
+# Define two functions for each tab
 with gr.Blocks() as demo:
-    with gr.Row():
-        name = gr.Textbox(label="Text")
-        pitch_btn = gr.Button("Pitch")
-        speed_btn = gr.Button("Speed")
-        emotion_btn = gr.Button("Emotion")
-        generate_btn = gr.Button("Generate")
-        output = gr.Audio(sources=["microphone", "upload"])
+    gr.Markdown(
+        """
+        # AI driven TTS and VC System
+        Convert Text to Speech and Clone your Voice with AI.
+        """)
+    with gr.Tab("Text To Speech"):
+        text_input = gr.Textbox()
+        text_output = gr.Textbox()
+        text_button = gr.Button("Convert")
+    with gr.Tab("Voice Clone"):
+        with gr.Row():
+            image_input = gr.Image()
+            image_output = gr.Image()
+        image_button = gr.Button("Clone")
+
+    text_button.click(flip_text, inputs=text_input, outputs=text_output)
+    image_button.click(flip_image, inputs=image_input, outputs=image_output)
 
 demo.launch()
