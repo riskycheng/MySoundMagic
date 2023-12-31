7350f8c5823a34ed4304550082acd70bbaf1c1ee
riskycheng <riskycheng@gmail.com>
2023-12-31 11:18:13 +0800

    able to generate the sound data, and models uploaded to \\192.168.31.164\sata11-176XXXX1768\Projects\MySoundMagic

--- MainGradio.py
+++ MainGradio.py
@@ -2,7 +2,9 @@ import gradio as gr
 from gtts import gTTS
 import os
 import numpy as np
+from Txt2SpeechUtils import Txt2SpeechUtils
 
+ttsEngine = Txt2SpeechUtils()
 
 def flip_text(input_text):
     return 'flip_text --> input_text'
@@ -20,16 +22,47 @@ with gr.Blocks() as demo:
         Convert Text to Speech and Clone your Voice with AI.
         """)
     with gr.Tab("Text To Speech"):
-        text_input = gr.Textbox()
-        text_output = gr.Textbox()
-        text_button = gr.Button("Convert")
+        gr.Markdown(
+            """
+            This is a demo of a text-to-speech demo, you may train your own model with your own voice,
+            with self-trained model, you can convert text to speech with your own voice.
+            """)
+        with gr.Tab("Train"):
+            gr.Markdown(
+                """
+                ### Train
+                Train a new model with your own voice.
+                """)
+            with gr.Column():
+                text_input = gr.Textbox(label="Voice data path")
+                text_output = gr.Textbox(label="Model save path")
+                button_train = gr.Button("Train")
+
+        # >>>>>>>> start conversion with TTS >>>>>>>>>>>>
+        with gr.Tab("Convert"):
+            gr.Markdown(
+                """
+                ### Convert
+                Convert text to speech with your trained model.
+                """)
+            with gr.Column():
+                model_input_selection_cvt = gr.Textbox(label="Model path")
+                text_input_prompt_cvt = gr.Textbox(label="Text prompt", lines=8)
+                text_output_cvt = gr.Textbox(label="Output audio")
+                audio_output_cvt = gr.Audio(label="output audio")
+                button_convert = gr.Button("Convert")
+
+    button_convert.click(ttsEngine.tts_fn, inputs=[
+        text_input_prompt_cvt,
+    ], outputs=[text_output_cvt, audio_output_cvt])
+
+    #  >>>>>>>>>>>>>>>>>>> Voice Clone fine-tuning >>>>>>>>>>>>>>>>>>>>>>>>>>>
     with gr.Tab("Voice Clone"):
         with gr.Row():
-            image_input = gr.Image()
-            image_output = gr.Image()
-        image_button = gr.Button("Clone")
+            vc_audio_input = gr.Audio(label="Input audio")
+            vc_audio_output = gr.Audio(label="Output audio")
+        button_vc_clone = gr.Button("Clone")
 
-    text_button.click(flip_text, inputs=text_input, outputs=text_output)
-    image_button.click(flip_image, inputs=image_input, outputs=image_output)
+    button_vc_clone.click(flip_text, inputs=vc_audio_input, outputs=vc_audio_output)
 
 demo.launch()
---
+++ Txt2SpeechUtils.py
@@ -0,0 +1,128 @@
+import os
+import logging
+import re_matching
+
+import torch
+import utils
+from infer import infer, latest_version, get_net_g
+import gradio as gr
+import webbrowser
+import numpy as np
+from config import config
+
+
+class Txt2SpeechUtils:
+    def __init__(self):
+        if config.webui_config.debug:
+            logger.info("Enable DEBUG-LEVEL log")
+            logging.basicConfig(level=logging.DEBUG)
+        self.hps = utils.get_hparams_from_file(config.webui_config.config_path)
+        version = self.hps.version if hasattr(self.hps, "version") else latest_version
+
+        self.device = config.webui_config.device
+        if self.device == "mps":
+            os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+
+        self.net_g = get_net_g(
+            model_path=config.webui_config.model, version=version, device=self.device, hps=self.hps
+        )
+
+        speaker_ids = self.hps.data.spk2id
+        speakers = list(speaker_ids.keys())
+        languages = ["ZH", "JP", "EN", "mix", "auto"]
+
+    def generate_audio(self,
+            slices,
+            sdp_ratio,
+            noise_scale,
+            noise_scale_w,
+            length_scale,
+            speaker,
+            language,
+    ):
+        audio_list = []
+        silence = np.zeros(self.hps.data.sampling_rate // 2, dtype=np.int16)
+        with torch.no_grad():
+            for piece in slices:
+                audio = infer(
+                    piece,
+                    sdp_ratio=sdp_ratio,
+                    noise_scale=noise_scale,
+                    noise_scale_w=noise_scale_w,
+                    length_scale=length_scale,
+                    sid=speaker,
+                    language=language,
+                    hps=self.hps,
+                    net_g=self.net_g,
+                    device=self.device,
+                )
+                audio16bit = gr.processing_utils.convert_to_16_bit_wav(audio)
+                audio_list.append(audio16bit)
+                audio_list.append(silence)  # 将静音添加到列表中
+        return audio_list
+
+    def tts_fn(self,
+               text,
+               speaker="Vo_Jay",
+               sdp_ratio=0.2,
+               noise_scale=0.8,
+               noise_scale_w=0.6,
+               length_scale=1.0,
+               language="ZH"):
+        audio_list = []
+        if language == "mix":
+            bool_valid, str_valid = re_matching.validate_text(text)
+            if not bool_valid:
+                return str_valid, (
+                    self.hps.data.sampling_rate,
+                    np.concatenate([np.zeros(self.hps.data.sampling_rate // 2)]),
+                )
+            result = re_matching.text_matching(text)
+            for one in result:
+                _speaker = one.pop()
+                for lang, content in one:
+                    audio_list.extend(
+                        generate_audio(
+                            content.split("|"),
+                            sdp_ratio,
+                            noise_scale,
+                            noise_scale_w,
+                            length_scale,
+                            _speaker,
+                            lang,
+                        )
+                    )
+        elif language.lower() == "auto":
+            sentences_list = split_by_language(text, target_languages=["zh", "ja", "en"])
+            for sentences, lang in sentences_list:
+                lang = lang.upper()
+                if lang == "JA":
+                    lang = "JP"
+                sentences = sentence_split(sentences, max=250)
+                for content in sentences:
+                    audio_list.extend(
+                        generate_audio(
+                            content.split("|"),
+                            sdp_ratio,
+                            noise_scale,
+                            noise_scale_w,
+                            length_scale,
+                            speaker,
+                            lang,
+                        )
+                    )
+        else:
+            audio_list.extend(
+                self.generate_audio(
+                    text.split("|"),
+                    sdp_ratio,
+                    noise_scale,
+                    noise_scale_w,
+                    length_scale,
+                    speaker,
+                    language,
+                )
+            )
+
+        audio_concat = np.concatenate(audio_list)
+        return "Success", (self.hps.data.sampling_rate, audio_concat)
